6e8a106281e89012212fba9f51ddf304e78171cc
Ethan Vrhel <ethanvrhel@gmail.com>
2024-04-08 14:02:47 -0700

    Changing how directory watching works on windows

--- src/ls_watch.c
+++ src/ls_watch.c
@@ -12,11 +12,6 @@
 #define NOTIF_BUFSIZE 1024
 #endif // LS_WINDOWS
 
-#define WATCH_ERROR -1 // thread error
-#define WATCH_RUNNING 0 // thread running
-#define WATCH_NOT_STARTED 1 // thread not started
-#define WATCH_CLOSING 2 // explicit close by user
-
 struct ls_watch_event_imp
 {
 	int action;
@@ -34,13 +29,16 @@ struct ls_watch
 {
 #if LS_WINDOWS
 	HANDLE hDirectory;
-
-	CRITICAL_SECTION cs;
-	CONDITION_VARIABLE cv;
+	OVERLAPPED ov;
+	HANDLE hThread; // thread that created this watch
 
 	int recursive;
 
-	int state; // thread state
+	union
+	{
+		FILE_NOTIFY_EXTENDED_INFORMATION fnei;
+		BYTE buf[NOTIF_BUFSIZE];
+	} u;
 
 	char source[MAX_PATH];
 	char target[MAX_PATH];
@@ -52,221 +50,65 @@ struct ls_watch
 
 #if LS_WINDOWS
 
-static DWORD WINAPI WatchThreadProc(LPVOID lpParam)
+static int dispatch_next_read(struct ls_watch *w)
 {
-	struct ls_watch *w = *(struct ls_watch **)lpParam;
-	struct ls_watch_event_imp *e, *next;
 	BOOL b;
+
+	b = ReadDirectoryChangesExW(
+		w->hDirectory,
+		w->u.buf,
+		NOTIF_BUFSIZE,
+		w->recursive,
+		FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_SECURITY,
+		NULL,
+		&w->ov,
+		NULL,
+		ReadDirectoryNotifyExtendedInformation);
+	
+	return b ? 0 : -1;
+}
+
+static int read_directory_changes(struct ls_watch *w)
+{
+	DWORD dwRet;
 	DWORD dwBytes;
-	HANDLE hDirectory;
-	int recursive;
 
-	union
+	dwRet = GetOverlappedResult(w->hDirectory, &w->ov, &dwBytes, FALSE);
+	if (!dwRet)
 	{
-		FILE_NOTIFY_EXTENDED_INFORMATION fnei;
-		BYTE buf[NOTIF_BUFSIZE];
-	} u;
+		if (ERROR_IO_INCOMPLETE == GetLastError())
+			return 1;
 
-	PFILE_NOTIFY_EXTENDED_INFORMATION pNotify;
-
-	EnterCriticalSection(&w->cs);
-
-	assert(w->state == WATCH_NOT_STARTED);
-
-	// save handle and flags
-
-	hDirectory = w->hDirectory;
-	recursive = w->recursive;
-
-	// signal that we're running
-	w->state = WATCH_RUNNING;
-	WakeAllConditionVariable(&w->cv);
-
-	do
-	{
-		// lock is held
-		LeaveCriticalSection(&w->cs);
-
-		ZeroMemory(&u.buf, NOTIF_BUFSIZE);
-
-		// read changes
-		b = ReadDirectoryChangesExW(
-			hDirectory,
-			u.buf,
-			NOTIF_BUFSIZE,
-			recursive,
-			FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_SECURITY,
-			&dwBytes,
-			NULL,
-			NULL,
-			ReadDirectoryNotifyExtendedInformation);
-		if (!b)
-		{
-			EnterCriticalSection(&w->cs);
-			w->state = WATCH_ERROR;
-			break;
-		}
-
-		EnterCriticalSection(&w->cs);
-		if (w->state == WATCH_CLOSING)
-			break;
-		LeaveCriticalSection(&w->cs);
-
-		// store event
-
-		e = ls_malloc(sizeof(struct ls_watch_event_imp));
-		if (!e)
-		{
-			EnterCriticalSection(&w->cs);
-			w->state = WATCH_ERROR;
-			break;
-		}
-
-		e->next = NULL;
-
-		ZeroMemory(e->source, MAX_PATH);
-		ZeroMemory(e->target, MAX_PATH);
-
-		switch (u.fnei.Action)
-		{
-		case FILE_ACTION_ADDED:
-			e->action = LS_WATCH_ADD;
-			ls_wchar_to_utf8_buf(u.fnei.FileName, e->source, MAX_PATH);
-			break;
-		case FILE_ACTION_REMOVED:
-			e->action = LS_WATCH_REMOVE;
-			ls_wchar_to_utf8_buf(u.fnei.FileName, e->source, MAX_PATH);
-			break;
-		case FILE_ACTION_MODIFIED:
-			e->action = LS_WATCH_MODIFY;
-			ls_wchar_to_utf8_buf(u.fnei.FileName, e->source, MAX_PATH);
-			break;
-		case FILE_ACTION_RENAMED_OLD_NAME:
-			if (u.fnei.NextEntryOffset == 0) // sanity check
-			{
-				// ignore
-				ls_free(e), e = NULL;
-				break;
-			}
-
-			pNotify = (PFILE_NOTIFY_EXTENDED_INFORMATION)(u.buf + u.fnei.NextEntryOffset);
-			if (pNotify->Action != FILE_ACTION_RENAMED_NEW_NAME)
-			{
-				// ignore
-				ls_free(e), e = NULL;
-				break;
-			}
-
-			e->action = LS_WATCH_RENAME;
-			ls_wchar_to_utf8_buf(u.fnei.FileName, e->source, MAX_PATH);
-			ls_wchar_to_utf8_buf(pNotify->FileName, e->target, MAX_PATH);
-
-			break;
-		case FILE_ACTION_RENAMED_NEW_NAME: // always follows OLD_NAME
-		default:
-			// ignore
-			ls_free(e), e = NULL;
-			break;
-		}
-
-		if (e) // add to queue
-		{
-			EnterCriticalSection(&w->cs);
-
-			if (w->back)
-				w->back->next = e;
-			else
-				w->front = e;
-
-			w->back = e;
-
-			WakeAllConditionVariable(&w->cv);
-		}
-		else
-			EnterCriticalSection(&w->cs);
-
-		// lock is held
-	} while (w->state == WATCH_RUNNING);
-
-	// lock is held
-
-	WakeAllConditionVariable(&w->cv); // prevent deadlock
-
-	// wait until user closes the watch
-	while (w->state == WATCH_RUNNING)
-		SleepConditionVariableCS(&w->cv, &w->cs, INFINITE);
-	LeaveCriticalSection(&w->cs);
-
-	// free all outstanding events
-	e = w->front;
-	while (e)
-	{
-		next = e->next;
-		ls_free(e);
-		e = next;
+		return -1;
 	}
-
-	w->front = NULL, w->back = NULL;
-
-	CloseHandle(hDirectory);
-	DeleteCriticalSection(&w->cs);	
-
-	ls_free(w);
 
 	return 0;
 }
 
 #endif // LS_WINDOWS
 
-static void LS_CLASS_FN ls_watch_dtor(struct ls_watch **pw)
+static void LS_CLASS_FN ls_watch_dtor(struct ls_watch *w)
 {
 #if LS_WINDOWS
-	struct ls_watch *w = *pw;
-
-	if (!w) return;
-
-	// signal thread to close
-	EnterCriticalSection(&w->cs);
-	w->state = WATCH_CLOSING;
-	WakeAllConditionVariable(&w->cv);
-	LeaveCriticalSection(&w->cs);
-
-	// thread will clean up the rest
+	if (w->hDirectory && w->hDirectory != INVALID_HANDLE_VALUE)
+		CloseHandle(w->hDirectory);
 #else
 #endif // LS_WINDOWS
 }
 
-static int LS_CLASS_FN ls_watch_wait(struct ls_watch **pw, unsigned long ms)
+static int LS_CLASS_FN ls_watch_wait(struct ls_watch *w, unsigned long ms)
 {
 #if LS_WINDOWS
-	struct ls_watch *w = *pw;
 	DWORD dwRet;
 
-	EnterCriticalSection(&w->cs);
+	dwRet = WaitForSingleObject(w->ov.hEvent, ms);
+	if (dwRet == WAIT_OBJECT_0)
+		return 0;
 
-	if (w->state != WATCH_RUNNING)
-	{
-		LeaveCriticalSection(&w->cs);
-		return -1;
-	}
-
-	if (!w->front)
-	{
-		dwRet = SleepConditionVariableCS(&w->cv, &w->cs, ms);
-		if (!dwRet)
-		{
-			// lock not held
-
-			if (ERROR_TIMEOUT == GetLastError())
-				return 1;
-
-			return -1;
-		}
-	}
-
-	LeaveCriticalSection(&w->cs);
-
-	return 0;
+	if (dwRet == ERROR_TIMEOUT)
+		return 1;
+	
+	return -1;
 #else
 	return -1;
 #endif // LS_WINDOWS
@@ -274,7 +116,7 @@ static int LS_CLASS_FN ls_watch_wait(struct ls_watch **pw, unsigned long ms)
 
 static const struct ls_class WatchClass = {
 	.type = LS_WATCH,
-	.cb = sizeof(struct ls_watch *),
+	.cb = sizeof(struct ls_watch),
 	.dtor = (ls_dtor_t)&ls_watch_dtor,
 	.wait = (ls_wait_t)&ls_watch_wait
 };
@@ -282,35 +124,17 @@ static const struct ls_class WatchClass = {
 ls_handle ls_watch_dir(const char *dir, int recursive)
 {
 #if LS_WINDOWS
-	struct ls_watch **h;
 	struct ls_watch *w;
 	WCHAR szPath[MAX_PATH];
-	HANDLE hThread;
 	int rc;
 
-	h = ls_handle_create(&WatchClass);
-	if (!h) return NULL;
-
-	*h = ls_calloc(1, sizeof(struct ls_watch));
-	if (!*h)
-	{
-		ls_close(h);
-		return NULL;
-	}
-
-	w = *h;
-	w->state = WATCH_NOT_STARTED;
-
-	InitializeCriticalSection(&w->cs);
-	InitializeConditionVariable(&w->cv);
+	w = ls_handle_create(&WatchClass);
+	if (!w) return NULL;
 
 	rc = ls_utf8_to_wchar_buf(dir, szPath, MAX_PATH);
 	if (!rc)
 	{
-		DeleteCriticalSection(&w->cs);
-		ls_free(w), *h = NULL;
-
-		ls_close(h);
+		ls_close(w);
 		return NULL;
 	}
 
@@ -321,33 +145,18 @@ ls_handle ls_watch_dir(const char *dir, int recursive)
 		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, NULL);
 	if (w->hDirectory == INVALID_HANDLE_VALUE)
 	{
-		DeleteCriticalSection(&w->cs);
-		ls_free(w), *h = NULL;
-
-		ls_close(h);
+		ls_close(w);
 		return NULL;
 	}
 
-	hThread = CreateThread(NULL, 0, &WatchThreadProc, h, 0, NULL);
-	if (!hThread)
+	rc = dispatch_next_read(w);
+	if (rc == -1)
 	{
-		CloseHandle(w->hDirectory);
-		DeleteCriticalSection(&w->cs);
-		ls_free(w), *h = NULL;
-
-		ls_close(h);
+		ls_close(w);
 		return NULL;
 	}
 
-	// wait for thread to initialize
-	EnterCriticalSection(&w->cs);
-	while (w->state == WATCH_NOT_STARTED)
-		SleepConditionVariableCS(&w->cv, &w->cs, INFINITE);
-	LeaveCriticalSection(&w->cs);
-
-	CloseHandle(hThread); // detach
-
-	return h;
+	return w;
 #else
 	return NULL;
 #endif // LS_WINDOWS
@@ -356,24 +165,17 @@ ls_handle ls_watch_dir(const char *dir, int recursive)
 int ls_watch_get_result(ls_handle watch, struct ls_watch_event *event)
 {
 #if LS_WINDOWS
-	struct ls_watch *w = *(struct ls_watch **)watch;
+	struct ls_watch *w = watch;
 	struct ls_watch_event_imp *e;
 	int action;
 
-	EnterCriticalSection(&w->cs);
-
 	if (!w->front)
-	{
-		LeaveCriticalSection(&w->cs);
 		return 1;
-	}
 
 	e = w->front;
 	w->front = w->front->next;
 	if (!w->front)
 		w->back = NULL;
-
-	LeaveCriticalSection(&w->cs);
 
 	action = e->action;
 	memcpy(w->source, e->source, MAX_PATH);
